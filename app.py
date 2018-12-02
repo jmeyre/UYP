e906f567333ff46792418566845e5602b20f10d1
Jack Titzman <jack_titzman@baylor.edu>
2018-12-02 13:23:46 -0600

    Changed register to apply. Hid apply

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for, flash, redirect
-from forms import RegistrationForm, LoginForm
+from forms import LoginForm
+# from forms import ApplyForm
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '2d7596cbe025473acb2bb5d8f268f5a5'
@@ -11,20 +12,22 @@ def home():
     return render_template('home.html')
 
 
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    form = RegistrationForm()
+""" (No need to apply online)
+@app.route('/apply', methods=['GET', 'POST'])
+def apply():
+    form = ApplyForm()
     if form.validate_on_submit():
         flash('Account created for {0}!'.format(form.username.data), 'success')
         return redirect(url_for('home'))
-    return render_template('register.html', title='Register', form=form)
+    return render_template('apply.html', title='Apply', form=form)
+"""
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == 'test@test.com' and form.password.data == 'password':
+        if form.username.data == 'username' and form.password.data == 'password':
             flash('You have been logged in!', 'success')
             return redirect(url_for('home'))
         else:
