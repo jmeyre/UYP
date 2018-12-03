7f2ae3c91f8a7c9cc39de35797181af6800898a6
benagueros <benagueros@gmail.com>
2018-12-03 13:39:27 -0600

    Working on Student Info Form. Not complete.

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, DateField, RadioField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 """ (No need to apply online)
@@ -17,3 +17,14 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+
+class StudentInfo(FlaskForm):
+    fName = StringField('First Name', validators=[DataRequired(), Length(min=2, max=20)])
+    mName = StringField('Middle Name', validators=[Length(min=2, max=20)])
+    lName = StringField('Last Name', validators=[DataRequired(), Length(min=2, max=20)])
+    suffix = StringField('Suffix', validators=[Length(min=1, max=3)])
+    """Might not need the length validator for DateField"""
+    bDay = DateField('Birthday', format='%d/%m/%Y', validators=(DataRequired(), Length(min=8, max=10)))
+    Gender = RadioField('Gender', choices=[('Male', 'Male'), ('Male', 'Male')], validators=[DataRequired()])
+
