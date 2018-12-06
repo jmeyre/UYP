20c12c7f253e35220aa77ba2a9045ee2ffa55477
John Eyre <jmeyre1998@gmail.com>
2018-12-06 15:07:07 -0600

    Fixed length of initial query id

--- InitialStaffQuery.sql
+++ InitialStaffQuery.sql
@@ -1,2 +1,2 @@
-# Create a new user with id: 0000001 and pass: password
-INSERT INTO users (ID, category, pword) VALUES ('0000001', 'Staff', '$2b$12$syivdenDKojWCsPxTU/q2ON2zI6BUBi6XnWXF50rclVTWklreAOci')
+# Create a new user with id: 000001 and pass: password
+INSERT INTO users (ID, category, pword) VALUES ('000001', 'Staff', '$2b$12$syivdenDKojWCsPxTU/q2ON2zI6BUBi6XnWXF50rclVTWklreAOci')
